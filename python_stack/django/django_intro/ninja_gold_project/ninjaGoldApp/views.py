6314d954df30b74660a6f1d9c2e99a1af1e0ca2d
Eric <eric.vasquez3327@gmail.com>
2021-03-18 12:33:07 -0500

    created cards for buttons

--- python_stack/django/django_intro/ninja_gold_project/ninjaGoldApp/views.py
+++ python_stack/django/django_intro/ninja_gold_project/ninjaGoldApp/views.py
@@ -1,5 +1,55 @@
 from django.shortcuts import render, redirect
+import random
+from datetime import datetime
 
 
 def index(request):
-    return render(request, 'index.html')
+
+    if 'yourGold' not in request.session:
+        request.session['yourGold'] = 0
+
+    if 'activity' not in request.session:
+        request.session['activity'] = []
+
+    context = {
+        'yourGold': request.session['yourGold'],
+        'activities': request.session['activity'],
+        }
+        
+    return render(request, 'index.html', context)
+
+
+def process_money(request):
+    now = datetime.now()
+
+    if 'farm' in request.POST:
+        place = 'Farm'
+        earned = random.randint(10,20)
+
+    if 'cave' in request.POST:
+        place = 'Cave'
+        earned = random.randint(5, 10)
+
+    if 'house' in request.POST:
+        place = 'House'
+        earned = random.randint(2, 5)
+
+    if 'casino' in request.POST:
+        place = "Casino"
+        earned = random.randint(-50, 50)
+
+    request.session['yourGold'] += earned
+    if earned < 0:
+        message = 'Entered a Casino and lost ' + str(earned*-1) + ' golds.....Ouch ('
+        request.session['activity'].append(message + now.strftime('%Y/%m/%d  %-I:%M %p)'))
+        request.session.save()        
+    else:
+        message = 'Earned ' + str(earned) + ' golds from the ' + place + '  ('
+        request.session['activity'].append(message + now.strftime('%Y/%m/%d  %-I:%M %p)'))
+        request.session.save()
+
+    return redirect('/')     
+
+def reset(request):
+    request.session.clear()
+    return redirect('/')
