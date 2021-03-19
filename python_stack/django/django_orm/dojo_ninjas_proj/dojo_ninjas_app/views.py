af31ddf8ebf22294d80a254045b22c7d2d783593
Eric <eric.vasquez3327@gmail.com>
2021-03-19 18:42:58 -0500

    able to add dojo and ninja

---
+++ python_stack/django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from .models import Dojo, Ninja
+
+
+# Create your views here.
+
+def index(request):
+    context = {
+        'all_dojos': Dojo.objects.all(),
+        'all_ninjas': Ninja.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def add_dojo(request):
+    Dojo.objects.create(
+        name = request.POST['name'],
+        city = request.POST['city'],
+        state = request.POST['state']
+    )
+
+    return redirect('/')
+
+def add_ninja(request):
+    dojo_name = request.POST['dojo']
+    Ninja.objects.create(
+        first_name = request.POST['first_name'],
+        last_name = request.POST['last_name'],
+        dojo = Dojo.objects.get(name=dojo_name)
+    )
+
+    return redirect('/')
+
+def delete_dojo(request):
+    dojo_name = request.POST['dojo']
+    dojo = Dojo.objects.get(name=dojo_name)
+    dojo_name.delete()
+
+    return redirect('/')
